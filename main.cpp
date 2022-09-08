6bd8f7dbb18467fcc45de0ba043aeb207ce3bca1
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-09-08 15:07:03 +0900

    5622번 문제 정답

--- main.cpp
+++ main.cpp
@@ -3,13 +3,31 @@
 
 using namespace std;
 
-
-
 int main()
 {
 	// iostream speed up
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
+	// variable declaration
+	string input;
+	int time{};
 
+	// input
+	cin >> input;
+
+	// change to num + calculating time
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] != 'Z')
+			if (input[i] - 'A' > 17)
+				time += ((input[i] - 'A' - 1) / 3 + 3);
+			else
+			time += ((input[i] - 'A') / 3) + 3;
+		else
+			time += 10;
+	}
+
+	// output
+	cout << time << endl;
 }
