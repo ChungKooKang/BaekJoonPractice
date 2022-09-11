aa9fe7890918c049df62304111689570f16a0a4f
rkdcndrnwg@gmail.com <rkdcndrnwg@gmail.com>
2022-09-11 15:01:04 +0900

    뇌를 자극하는 C++ stl 공부 중!

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	// iostream speed up
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
 
-	// variable declaration
-	string input;
-	int count{};
+    vector<int> v;
+    cout << "== 원소 추가 전 ==" << endl << endl;
+    cout << "원소를 추가하기 전" << endl
+        << "size()     : " << v.size() << endl
+        << "capacity() : " << v.capacity() << endl
+        << "max_size() : " << v.max_size() << endl << endl;
 
-	// input
-	cin >> input;
+    cout << "== 원소 추가 ==" << endl << endl;
 
-	// checking 크로아티아
-	for (int i = 0; i < input.length(); i++)
-	{
-		input.replace()
-	}
+    for (int i = 1; i < 10; i++)
+    {
+        v.push_back(i);
+        cout << "push_back(" << i <<") 추가 후" << endl
+            << "size()     : " << v.size() << endl
+            << "capacity() : " << v.capacity() << endl
+            << "max_size() : " << v.max_size() << endl << endl;
+    }
 
-	// counting
+    cout << "== 원소 제거 ==" << endl << endl;
 
-	// output
+    for (int i = 1; i < 10; i++)
+    {
+        v.pop_back();
+        cout << "pop_back(" << 10 - i << ") 제거 후" << endl
+            << "size()     : " << v.size() << endl
+            << "capacity() : " << v.capacity() << endl
+            << "max_size() : " << v.max_size() << endl << endl;
+    }
 
 }
+
