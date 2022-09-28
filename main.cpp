19434241080b339430442e7f337033d2389841b8
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-09-28 18:58:43 +0900

    2941번 내가 한 통과 정답 (2022.09.28)

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int main()
 {
-	vector<string> croatian = { "c=", "c-", "dz=","d-", "lj", "nj", "s=", "z="};
+	vector<string> alphabat {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 	int idx;
 	string input;
 	cin >> input;
 
-	for (int i = 0; i < croatian.size(); i++)
+	for (int i = 0; i < alphabat.size(); i++)
 	{
 		while (1)
 		{
-			idx = input.find(croatian[i]);	// string::find function is used to find the first character of the first match.
+			idx = input.find(alphabat[i]);
 			if (idx == string::npos) break;
 
-			input.replace(idx, croatian[i].length(), "#");
+			input.replace(idx, alphabat[i].length(), "#");
 		}
 	}
 	cout << input.length();
